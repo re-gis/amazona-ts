55edcc6437dde9a5c716c2ec45885e90ee97c6d7
D Regis <irumvaregisdmc@gmail.com>
2023-06-20 14:54:19 +0200

    Making shipping address page

--- backend/src/routes/user.router.ts
+++ backend/src/routes/user.router.ts
@@ -1,6 +1,6 @@
 import expressAsyncHandler from "express-async-handler";
 import express, { Request, Response } from "express";
-import { UserModel } from "../models/user.model";
+import { User, UserModel } from "../models/user.model";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../utils";
 
@@ -23,8 +23,32 @@ userRouter.post(
           token: generateToken(user),
         });
       }
-    } 
+    }
 
     return res.status(401).send({ message: "Invalid email or password" });
+  })
+);
+
+userRouter.post(
+  "/signup",
+  expressAsyncHandler(async (req: Request, res: Response) => {
+    const u = await UserModel.findOne({ email: req.body.email });
+    if (u)
+      return res
+        .status(401)
+        .send({ message: `${req.body.email} already used!` });
+    const user = await UserModel.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    } as User);
+
+    return res.status(201).send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user),
+    });
   })
 );
